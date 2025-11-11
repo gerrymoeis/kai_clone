79a5eca81286b27817bea3e135423e81b4778a09
gerrymoeis <gerrymoeis@gmail.com>
2025-11-11 21:35:31 +0700

    fix link gotailwindcss

--- build.sh
+++ build.sh
@@ -27,12 +27,13 @@ echo "templ installed"
 
 # 2. Download and install gotailwindcss
 echo "Downloading gotailwindcss..."
-TAILWIND_VERSION="v2.1.4"
-# GitHub releases use format: gotailwindcss_v2.1.4_Linux_x86_64.tar.gz
+# Correct repository: github.com/gotailwindcss/tailwind
+TAILWIND_VERSION="v0.5.0"
 OS_CAPS="Linux"
-TAILWIND_URL="https://github.com/bep/gotailwindcss/releases/download/${TAILWIND_VERSION}/gotailwindcss_${TAILWIND_VERSION}_${OS_CAPS}_${ARCH_ORIG}.tar.gz"
+TAILWIND_URL="https://github.com/gotailwindcss/tailwind/releases/download/${TAILWIND_VERSION}/tailwindcss-extra_${TAILWIND_VERSION}_${OS_CAPS}_${ARCH_ORIG}.tar.gz"
 wget -q -O tailwind.tar.gz "$TAILWIND_URL"
-tar -xzf tailwind.tar.gz gotailwindcss
+tar -xzf tailwind.tar.gz tailwindcss-extra
+mv tailwindcss-extra gotailwindcss
 chmod +x gotailwindcss
 rm tailwind.tar.gz
 echo "gotailwindcss installed"
